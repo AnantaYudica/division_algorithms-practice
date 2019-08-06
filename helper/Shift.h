b16caa4a39d6b7e7b696c3d8d4f1f279a560ec9b
Ananta Yudica <nta040689@gmail.com>
2019-08-06 15:12:58 +0700

    Fixed index array out of bounds and cast type

--- helper/Shift.h
+++ helper/Shift.h
@@ -19,10 +19,11 @@ void Shift(std::uint8_t (&a)[N], int s)
         for (std::size_t i = N - 1, j = N - (s + 1); i < N;)
         {
             if (b == 0)
-                a[i--] = (j >= N ? 0 : a[j--]);
+                a[i--] = (j >= N ? std::uint8_t(0) : a[j--]);
             else
-                a[i--] = std::uint8_t(a[j--] << b) | ((j >= N) ? std::uint8_t(0) : 
-                    std::uint8_t(a[j] >> (8 - b)));
+                a[i--] = (((j >= N) ? std::uint8_t(0) : 
+                    std::uint8_t(a[j--] << b)) | ((j >= N) ? std::uint8_t(0) : 
+                    std::uint8_t(a[j] >> (8 - b))));
         }
     }
     else if (s < 0 || b < 0)
@@ -30,10 +31,11 @@ void Shift(std::uint8_t (&a)[N], int s)
         for (std::size_t i = 0, j = -s; i < N;)
         {
             if (b == 0)
-                a[i++] = (j >= N ? 0 : a[j++]);
+                a[i++] = (j >= N ? std::uint8_t(0) : a[j++]);
             else
-                a[i++] = std::uint8_t(a[j++] >> -b) | ((j >= N) ? std::uint8_t(0) : 
-                    std::uint8_t(a[j] << (8 + b)));
+                a[i++] = (((j >= N) ? std::uint8_t(0) : 
+                    std::uint8_t(a[j++] >> -b)) | ((j >= N) ? std::uint8_t(0) : 
+                    std::uint8_t(a[j] << (8 + b))));
         }
     }
 }
